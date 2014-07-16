859a41123d7457e9b45187f02b83ddc9d63bb21c
codemonkey-uk <codemonkey.uk@gmail.com>
2014-07-16 14:48:49 +0100

    triangle batcher moved to own file

--- src/CApp_OnRender.cpp
+++ src/CApp_OnRender.cpp
@@ -4,79 +4,10 @@
 #include "shape.h"
 #include "geometry_constants.h"
 #include "font.h"
+#include "trianglebatcher.h"
 
 #include <algorithm>
 #include <stdio.h>
-
-class TriangleBatcher
-{
-	public:
-		TriangleBatcher(GLuint program, int traingleCapacity)
-            : mProgram(program)
-            , mMaxVert(traingleCapacity*3)
-            , mVertArray( new GLfloat[mMaxVert*2] )
-			, mVertCount(0)
-        {
-			mTransformUniform = glGetUniformLocation(mProgram, "transform");
-			if (mTransformUniform==-1) printf("Transform Uniform not found.\n");
-			mColourUniform = glGetUniformLocation(mProgram, "colour");
-			if (mColourUniform==-1) printf("Colour Uniform not found.\n");
-        }
-        
-        ~TriangleBatcher()
-        {
-        	delete [] mVertArray;
-        }
-        
-        void AddTriangles(const GLfloat* verts, int tc)
-        {
-			int vc = tc*3;
-        	if (mVertCount + vc < mMaxVert)
-        	{
-	        	memcpy(mVertArray+mVertCount*2, verts, vc*2*sizeof(GLfloat));
-    	    	mVertCount += vc;
-    	    }
-        }
-        
-        void Flush(const Entropy::GFX::Colour& col) 
-		{
-			Geometry::MatrixN<float,4> identity;
-			Flush( identity, col );
-		}
-		
-		void Flush(
-			const Geometry::MatrixN<float,4>& matrix, 
-			const Entropy::GFX::Colour& col) 
-		{
-			if (mVertCount>0)
-			{			
-				// Use the program object 
-				glUseProgram(mProgram);
-			
-				glUniformMatrix4fv( mTransformUniform, 1, GL_FALSE, matrix[0] );			
-				glUniform4f(mColourUniform, col.mR, col.mG, col.mB, 1.0f);
-			
-				glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, mVertArray); 
-				glEnableVertexAttribArray(0);
-		 
-				glDrawArrays(mMode, 0, mVertCount);	
-			
-				mVertCount = 0;	
-			}
-		}
-		
-	private:
-		// all set up in the initialiser list of the Shape constructor
-		static const GLenum mMode = GL_TRIANGLES;
-		const GLint mMaxVert;
-		GLfloat * const mVertArray;
-		
-		GLint mVertCount;
-
-		GLint mProgram;
-		int mTransformUniform;
-		int mColourUniform;
-};
 
 void CApp::PrintString( const char* buffer, int h, int v, HAlign align)	
 {
---
+++ src/trianglebatcher.h
@@ -0,0 +1,36 @@
+#ifndef TRIANGLE_BATCHER_H_INCLUDED
+#define TRIANGLE_BATCHER_H_INCLUDED
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#include "matrixn.h"
+#include "colour.h"
+
+class TriangleBatcher
+{
+	public:
+		TriangleBatcher(GLuint program, int traingleCapacity);
+        
+        ~TriangleBatcher();
+        
+        void AddTriangles(const GLfloat* verts, int tc);
+        
+        void Flush(const Entropy::GFX::Colour& col);
+		void Flush(
+			const Geometry::MatrixN<float,4>& matrix, 
+			const Entropy::GFX::Colour& col);
+		
+	private:
+		static const GLenum mMode = GL_TRIANGLES;
+		const GLint mMaxVert;
+		GLfloat * const mVertArray;
+		
+		GLint mVertCount;
+
+		GLint mProgram;
+		int mTransformUniform;
+		int mColourUniform;
+};
+
+#endif//TRIANGLE_BATCHER_H_INCLUDED
